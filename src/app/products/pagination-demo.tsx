586cca53d2b84803c24cd2a07685e73b3f04b1ad
Sudip-gt <sumanledop@gmail.com>
2025-04-10 23:41:06 +0545

    Fixed ESLint errors again

--- src/app/products/pagination-demo.tsx
+++ src/app/products/pagination-demo.tsx
@@ -1,53 +1,54 @@
 "use client"
 import {
-    Pagination,
-    PaginationContent,
-    PaginationEllipsis,
-    PaginationItem,
-    PaginationLink,
-    PaginationNext,
-    PaginationPrevious,
-  } from "@/components/ui/pagination"
+  Pagination,
+  PaginationContent,
+  PaginationEllipsis,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+} from "@/components/ui/pagination"
 import { useSearchParams } from "next/navigation";
 import { useCallback } from "react";
-  
-  export function PaginationDemo() {
-    const searchParams = useSearchParams()
-   
-    // Get a new searchParams string by merging the current
-    // searchParams with a provided key/value pair
-    const createQueryString = useCallback(
-      (name: string, value: string) => {
-        const params = new URLSearchParams(searchParams.toString())
-        params.set(name, value)
-   
-        return params.toString()
-      },
-      [searchParams]
-    )
-   
-    return (
-      <Pagination>
-        <PaginationContent>
-          <PaginationItem>
-            <PaginationPrevious href="#" />
-          </PaginationItem>
-          {["1", "2", "3"].map((a) => (
-               <PaginationItem key={a}>
-               <PaginationLink href={"/products" + "?" + createQueryString("page", a)}
-                isActive = {searchParams.get("page") === a}> {a} </PaginationLink>
-             </PaginationItem>
-          ))}
-          <PaginationItem>
-            <PaginationEllipsis />
-          </PaginationItem>
-          <PaginationItem>
-            <PaginationNext href="#" />
-          </PaginationItem>
-        </PaginationContent>
-      </Pagination>
-    )
-  }
-  
 
-  // key={item.id || item.name}
+export function PaginationDemo() {
+  const searchParams = useSearchParams() as URLSearchParams; // Casting to URLSearchParams
+
+  // Get a new searchParams string by merging the current
+  // searchParams with a provided key/value pair
+  const createQueryString = useCallback(
+    (name: string, value: string) => {
+      const params = new URLSearchParams(searchParams.toString())
+      params.set(name, value)
+
+      return params.toString()
+    },
+    [searchParams]
+  )
+
+  return (
+    <Pagination>
+      <PaginationContent>
+        <PaginationItem>
+          <PaginationPrevious href="#" />
+        </PaginationItem>
+        {["1", "2", "3"].map((a) => (
+          <PaginationItem key={a}>
+            <PaginationLink
+              href={"/products" + "?" + createQueryString("page", a)}
+              isActive={searchParams.get("page") === a}
+            >
+              {a}
+            </PaginationLink>
+          </PaginationItem>
+        ))}
+        <PaginationItem>
+          <PaginationEllipsis />
+        </PaginationItem>
+        <PaginationItem>
+          <PaginationNext href="#" />
+        </PaginationItem>
+      </PaginationContent>
+    </Pagination>
+  )
+}
