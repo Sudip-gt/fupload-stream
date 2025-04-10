33b184a086a17cc586fe227612ac9130b9b4c31f
Sudip-gt <sumanledop@gmail.com>
2025-04-10 23:58:15 +0545

    Fixed ESLint errors again

--- src/app/products/pagination-demo.tsx
+++ src/app/products/pagination-demo.tsx
@@ -1,54 +1,55 @@
 "use client"
 import {
-  Pagination,
-  PaginationContent,
-  PaginationEllipsis,
-  PaginationItem,
-  PaginationLink,
-  PaginationNext,
-  PaginationPrevious,
-} from "@/components/ui/pagination"
+    Pagination,
+    PaginationContent,
+    PaginationEllipsis,
+    PaginationItem,
+    PaginationLink,
+    PaginationNext,
+    PaginationPrevious,
+  } from "@/components/ui/pagination"
 import { useSearchParams } from "next/navigation";
 import { useCallback } from "react";
+  
+  export function PaginationDemo() {
+    const searchParams = useSearchParams()
 
-export function PaginationDemo() {
-  const searchParams = useSearchParams() as URLSearchParams; // Casting to URLSearchParams
-
-  // Get a new searchParams string by merging the current
-  // searchParams with a provided key/value pair
-  const createQueryString = useCallback(
-    (name: string, value: string) => {
-      const params = new URLSearchParams(searchParams.toString())
-      params.set(name, value)
-
-      return params.toString()
-    },
-    [searchParams]
-  )
-
-  return (
-    <Pagination>
-      <PaginationContent>
-        <PaginationItem>
-          <PaginationPrevious href="#" />
-        </PaginationItem>
-        {["1", "2", "3"].map((a) => (
-          <PaginationItem key={a}>
-            <PaginationLink
-              href={"/products" + "?" + createQueryString("page", a)}
-              isActive={searchParams.get("page") === a}
-            >
-              {a}
-            </PaginationLink>
+    const page = searchParams.get("page");
+   
+    // Get a new searchParams string by merging the current
+    // searchParams with a provided key/value pair
+    const createQueryString = useCallback(
+      (name: string, value: string) => {
+        const params = new URLSearchParams(searchParams.toString())
+        params.set(name, value)
+   
+        return params.toString()
+      },
+      [searchParams]
+    )
+   
+    return (
+      <Pagination>
+        <PaginationContent>
+          <PaginationItem>
+            <PaginationPrevious href="#" />
           </PaginationItem>
-        ))}
-        <PaginationItem>
-          <PaginationEllipsis />
-        </PaginationItem>
-        <PaginationItem>
-          <PaginationNext href="#" />
-        </PaginationItem>
-      </PaginationContent>
-    </Pagination>
-  )
-}
+          {["1", "2", "3"].map((a) => (
+               <PaginationItem key={a}>
+               <PaginationLink href={"/products" + "?" + createQueryString("page", a)}
+                isActive = {searchParams.get("page") === a}> {a} </PaginationLink>
+             </PaginationItem>
+          ))}
+          <PaginationItem>
+            <PaginationEllipsis />
+          </PaginationItem>
+          <PaginationItem>
+            <PaginationNext href="#" />
+          </PaginationItem>
+        </PaginationContent>
+      </Pagination>
+    )
+  }
+  
+
+  //
